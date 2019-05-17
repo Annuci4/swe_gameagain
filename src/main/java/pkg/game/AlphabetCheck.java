4ae4bc9eca8151417d407edf3f62d07e274331b0
Annuci4 <36335608+Annuci4@users.noreply.github.com>
2019-05-17 18:33:35 +0200

    Add files via upload

---
+++ src/main/java/pkg/game/AlphabetCheck.java
@@ -0,0 +1,21 @@
+package pkg.game;
+
+public class AlphabetCheck {
+    /**
+     * TableCheck which checks the correctness of the given coordinate point.
+     * @param helper the given coordinate point, what we check.
+     * @return {@code true}, if the coordinate is not a number.
+     */
+    public static boolean alphabetCheck(String helper){
+        boolean fool=false;
+        for (int i = 0; i < helper.length(); i++) {
+            if(Character.isAlphabetic(helper.charAt(i))||!Character.isDigit(helper.charAt(i))) {
+                fool = true;
+            }else{
+                if(helper.length()>2)
+                    fool=true;
+            }
+        }
+        return fool;
+    }
+}
